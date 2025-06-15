32e5d01aa9db046e48b5aa161176f4c163bfdde3
Juanda999 <juanda999games@gmail.com>
2025-06-15 00:18:53 -0500

    Set default deck based on the card presset

--- Assets/Scripts/Managers/CardManager.cs
+++ Assets/Scripts/Managers/CardManager.cs
@@ -6,16 +6,23 @@ public class CardManager : MonoBehaviour
 {
     public static CardManager instance;
 
+    [SerializeField] private CardGenerationPresset cardGenerationPresset;
     public List<Card> cards;
 
     void Awake()
     {
         instance = this;
         cards = new List<Card>();
+
+        cards = cardGenerationPresset.allCards;
     }
 
     public void AddCartToList(Card card)
     {
         cards.Add(card);
     }
+
+
+
+
 }
