c9444e024ce956f20a3b458f4a25f449b5b9d184
Juanda999 <juanda999games@gmail.com>
2025-06-15 00:18:31 -0500

    Add card generation presset

---
+++ Assets/Scriptables/CardGenerationPresset/CardGenerationPresset.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Card Generation Presset", menuName = "Scriptables/Card Generation Presset")]
+public class CardGenerationPresset : ScriptableObject
+{
+    public List<Card> allCards;
+
+    [ContextMenu("Generate Random Deck")]
+    private void GenerateBasicDeck()
+    {
+        allCards = new List<Card>();
+
+        Suit[] suits = new Suit[] { Suit.Diamond, Suit.Hearth, Suit.Spades, Suit.Clover };
+        for (int i = 0; i < suits.Length; i++)
+        {
+            for (int j = 1; j < 15; j++)
+            {
+                Card card = new Card();
+                card.SetCardNumber(j);
+                card.cardSuit = suits[i];
+                card.SetCardFace(card);
+
+                allCards.Add(card);
+            }
+        }
+    }
+}
