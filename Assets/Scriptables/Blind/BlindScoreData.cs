4186d5ead6ca82b8c7e2ee2f62f5129c85d13644
Juanda999 <juanda999games@gmail.com>
2025-06-24 22:31:00 -0500

    Create basic score setter

---
+++ Assets/Scriptables/Blind/BlindScoreData.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BlindScoreData", menuName = "Scriptables/Blind/Blind Score Data")]
+public class BlindScoreData : ScriptableObject
+{
+    public int[] baseScore;
+    public float[] scoreMultiplier;
+}
---
+++ Assets/Scripts/Managers/BlindManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class BlindManager : MonoBehaviour
+{
+    public static BlindManager instance;
+    public int anteLevel;
+    [SerializeField] private int requiredScore;
+    [SerializeField] private BlindScoreData blindScoreData;
+    [SerializeField] private int currentBlindProgress; //0 to 2;
+
+    [Header("UI")]
+    [SerializeField] private TextMeshProUGUI requiredScoreText;
+    void Awake()
+    {
+        instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetRequiredScore();
+    }
+
+    public void SetRequiredScore()
+    {
+        requiredScore = Mathf.RoundToInt(blindScoreData.baseScore[anteLevel] * blindScoreData.scoreMultiplier[currentBlindProgress]);
+        requiredScoreText.text = requiredScore.ToString();
+    }
+
+}
