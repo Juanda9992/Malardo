c1c85bde2a53a2fe7e98407343ca9288d55c4a90
Juanda999 <juanda999games@gmail.com>
2025-06-26 00:17:37 -0500

    Add game event played

---
+++ Assets/Scripts/Managers/GameEventsManager.cs
@@ -0,0 +1,37 @@
+using System;
+using UnityEngine;
+
+public class GameEventsManager : MonoBehaviour
+{
+    public static GameEventsManager instance;
+
+    [SerializeField] private bool debugger;
+    public event Action OnHandPlayed;
+    public event Action OnHandDiscarted;
+    public event Action<Card> OnCardPlay;
+
+    void Awake()
+    {
+        instance = this;
+    }
+    public void TriggerHandPlayed()
+    {
+        OnHandPlayed?.Invoke();
+
+        if (debugger) Debug.Log("Hand Played");
+    }
+
+    public void TriggerHandDiscard()
+    {
+        OnHandDiscarted?.Invoke();
+
+        if (debugger) Debug.Log("Hand Discard");
+    }
+
+    public void TriggerCardPlayed(Card card)
+    {
+        OnCardPlay?.Invoke(card);
+        
+        if (debugger) card.DegubCardInfo();
+    } 
+}
