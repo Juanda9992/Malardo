bde3b8c206149c5f1728bdb77e3e4e03f1249184
Juanda999 <juanda999games@gmail.com>
2025-07-13 13:15:53 -0500

    Give chips by discard

---
+++ Assets/Scriptables/Jokers/Code/Effects/Dynamic Effects/GiveChipsPerDiscard.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Give Chips By Discard", menuName = "Scriptables/Joker/Effect/Give Chips by Discard")]
+public class GiveChipsPerDiscard : JokerEffect
+{
+    public int chipsPerDiscard;
+    public override void ApplyEffect()
+    {
+        chipsPerDiscard = ammount * GameStatusManager._Status.discardsRemaining;
+        ScoreManager.instance.AddChips(chipsPerDiscard);
+        Debug.Log("Chips given: " + chipsPerDiscard);
+    }
+
+    public override string GetCustomMessage()
+    {
+        return "+" + chipsPerDiscard;
+    }
+}
