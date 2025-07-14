9a483e76f4e3d05b4fcf7645e77fb499b4efcbe5
Juanda999 <juanda999games@gmail.com>
2025-07-14 17:09:25 -0500

    GIve chips by currency effect

---
+++ Assets/Scriptables/Jokers/Code/Effects/Chips/GiveChipsByCurrency.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+[CreateAssetMenu(fileName = "Give Chips By Currency",menuName = "Scriptables/Joker/Effect/Give Chips by Currency")]
+public class GiveChipsByCurrency : JokerEffect
+{
+    public int chipAmmount;
+    public override void ApplyEffect()
+    {
+        ScoreManager.instance.AddChips(chipAmmount * CurrencyManager.instance.currentCurrency);
+    }
+
+    public override string GetCustomMessage()
+    {
+        return (chipAmmount * CurrencyManager.instance.currentCurrency).ToString();
+    }
+}
