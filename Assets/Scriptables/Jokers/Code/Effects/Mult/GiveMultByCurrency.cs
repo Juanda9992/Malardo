314908b4571555d7f970692e6ea837fe345a7948
Juanda999 <juanda999games@gmail.com>
2025-07-14 17:23:06 -0500

    Give mult by currency

---
+++ Assets/Scriptables/Jokers/Code/Effects/Mult/GiveMultByCurrency.cs
@@ -0,0 +1,18 @@
+
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Give Mult by Currency", menuName = "Scriptables/Joker/Effect/Give Mult by Currency")]
+public class GiveMultByCurrency : JokerEffect
+{
+    public int divider;
+    public int multValue;
+    public override void ApplyEffect()
+    {
+        ScoreManager.instance.AddMult(Mathf.Ceil(CurrencyManager.instance.currentCurrency / divider));
+    }
+
+    public override string GetCustomMessage()
+    {
+        return (Mathf.Ceil(CurrencyManager.instance.currentCurrency / divider) * multValue).ToString();
+    }
+}
