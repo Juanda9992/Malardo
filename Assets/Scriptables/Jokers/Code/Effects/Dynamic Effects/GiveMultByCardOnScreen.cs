614534d02a20bf1f5b6c7a8f0b9772df1d8f57e7
Juanda999 <juanda999games@gmail.com>
2025-07-14 22:53:30 -0500

    Give mult by remaining cards on screen

---
+++ Assets/Scriptables/Jokers/Code/Effects/Dynamic Effects/GiveMultByCardOnScreen.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+[CreateAssetMenu(fileName = "Give Mult by Required Card", menuName = "Scriptables/Joker/Effect/Give Mult per Required Card")]
+public class GiveMultByCardOnScreen : JokerEffect
+{
+    public int multammount;
+    public int requiredNumber;
+    public override void ApplyEffect()
+    {
+        if (CardPlayer.instance.currentHand.Count > 0)
+        {
+            ScoreManager.instance.AddMult(CardPlayer.instance.currentHand.FindAll(x => x.number == requiredNumber).Count * multammount);
+        }
+    }
+
+    public override string GetCustomMessage()
+    {
+        return "+"+(CardManager.instance.cardsOnScreen.FindAll(x => x.currentCard.number == requiredNumber).Count * multammount);
+    }
+}
