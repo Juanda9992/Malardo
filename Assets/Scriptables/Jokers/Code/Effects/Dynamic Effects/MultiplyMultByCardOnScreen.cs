b716dc8a9014eb247c42c33f47400da57fd21bfa
Juanda999 <juanda999games@gmail.com>
2025-07-14 23:17:09 -0500

    Triboulet Joker

--- Assets/Scriptables/Jokers/Code/Effects/Dynamic Effects/MultiplyMultByCardOnScreen.cs
+++ Assets/Scriptables/Jokers/Code/Effects/Dynamic Effects/MultiplyMultByCardOnScreen.cs
@@ -3,18 +3,31 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Multiply Multi per Card On Screen", menuName = "Scriptables/Joker/Effect/Multiply Multi by Card on Screen")]
 public class MultiplyMultByCardOnScreen : JokerEffect
 {
-    public int requiredNumber;
+    public int[] requiredNumbers;
     public float multiplier;
     public override void ApplyEffect()
     {
         if (CardPlayer.instance.currentHand.Count > 0)
         {
-            ScoreManager.instance.MultiplyMulti(CardManager.instance.cardsOnScreen.FindAll(x => x.currentCard.number == requiredNumber).Count * multiplier);
+            int founds = 0;
+
+            for (int i = 0; i < requiredNumbers.Length; i++)
+            {
+                founds += CardManager.instance.cardsOnScreen.FindAll(x => x.currentCard.number == requiredNumbers[i]).Count;
+            }
+            ScoreManager.instance.MultiplyMulti(founds * multiplier);
         }
     }
 
+
     public override string GetCustomMessage()
     {
-        return "X" + CardManager.instance.cardsOnScreen.FindAll(x => x.currentCard.number == requiredNumber).Count * multiplier;
+        int founds = 0;
+
+        for (int i = 0; i < requiredNumbers.Length; i++)
+        {
+            founds += CardManager.instance.cardsOnScreen.FindAll(x => x.currentCard.number == requiredNumbers[i]).Count;
+        }
+        return "X" + founds * multiplier;
     }
 }
