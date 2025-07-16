7726a09d38b3574959ff1505a6e4b3bbf5197a74
Juanda999 <juanda999games@gmail.com>
2025-07-16 00:02:31 -0500

    Animate jokers on pointer

--- Assets/Scripts/Joker/JokerHover.cs
+++ Assets/Scripts/Joker/JokerHover.cs
@@ -1,17 +1,20 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
-
+using DG.Tweening;
 public class JokerHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    [SerializeField] private float yOffset;
+    [SerializeField] private float hoverScale;
+    [SerializeField] private float descriptionYOffset;
     [SerializeField] private JokerContainer jokerContainer;
     public void OnPointerEnter(PointerEventData data)
     {
-        JokerDescription.instance.SetDescriptionOn(jokerContainer._joker, (Vector2)transform.position + new Vector2(0,yOffset));
+        transform.DOScale(hoverScale, 0.3f);
+        JokerDescription.instance.SetDescriptionOn(jokerContainer._joker, (Vector2)transform.position + new Vector2(0,descriptionYOffset));
     }
 
     public void OnPointerExit(PointerEventData data)
     {
+        transform.DOScale(1, 0.2f);
         JokerDescription.instance.SetDescriptionOff();
     }
 }
