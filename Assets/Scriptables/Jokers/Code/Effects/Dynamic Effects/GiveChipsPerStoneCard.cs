6ee5c939837d9b0219c4731d250b7f9ebd0deaa4
Juanda999 <juanda999games@gmail.com>
2025-07-18 00:13:18 -0500

    Give chips per stone card effect

---
+++ Assets/Scriptables/Jokers/Code/Effects/Dynamic Effects/GiveChipsPerStoneCard.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Give Chips per StoneCard",menuName = "Scriptables/Joker/Effect/Give Chips per Stone Card")]
+public class GiveChipsPerStoneCard : JokerEffect
+{
+    public override void ApplyEffect()
+    {
+        ScoreManager.instance.AddChips((int)ammount * DeckManager.instance.GetAllStoneCardsOnFullDeck());
+    }
+
+    public override string GetCustomMessage()
+    {
+        return "+"+((int)ammount * DeckManager.instance.GetAllStoneCardsOnFullDeck()).ToString();
+    }
+}
