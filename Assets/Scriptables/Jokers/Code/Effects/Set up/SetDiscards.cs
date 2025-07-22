1919a546e6c2c2ea44f4b12a679261d98535e676
Juanda999 <juanda999games@gmail.com>
2025-07-21 23:08:20 -0500

    Set up discards and hands effects

---
+++ Assets/Scriptables/Jokers/Code/Effects/Set up/SetDiscards.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Set Doscards", menuName = "Scriptables/Joker/Effect/Set up/Set Doscards")]
+public class SetDiscards : JokerEffect
+{
+    public int discardAmmount;
+    public bool addAmmount;
+    public override void ApplyEffect()
+    {
+        if (addAmmount)
+        {
+            HandManager.instance.SetDefaultDiscards(HandManager.instance.defaultHands + discardAmmount);
+        }
+        else
+        {
+            HandManager.instance.SetDefaultDiscards(discardAmmount);
+        }
+    }
+}
---
+++ Assets/Scriptables/Jokers/Code/Effects/Set up/SetHands.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Set Doscards", menuName = "Scriptables/Joker/Effect/Set up/Set Hands")]
+public class SetHands : JokerEffect
+{
+    public int handAmmount;
+    public bool addAmmount;
+    public override void ApplyEffect()
+    {
+        if (addAmmount)
+        {
+            HandManager.instance.SetDefaultHands(HandManager.instance.defaultHands+ handAmmount);
+        }
+        else
+        {
+            HandManager.instance.SetDefaultHands(handAmmount);
+        }
+    }
+}
