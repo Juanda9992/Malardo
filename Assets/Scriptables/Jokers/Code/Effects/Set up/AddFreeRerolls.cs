068ac47af15dc8407ad8024cf1aa0ccd5bc3898c
Juanda999 <juanda999games@gmail.com>
2025-07-22 19:16:46 -0500

    Add free rerolls effect

---
+++ Assets/Scriptables/Jokers/Code/Effects/Set up/AddFreeRerolls.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+[CreateAssetMenu(fileName = "Add Free Rerolls",menuName = "Scriptables/Joker/Effect/Set up/Add Free Rerolls")]
+public class AddFreeRerolls : JokerEffect
+{
+    public int rerollAmount;
+    public override void ApplyEffect()
+    {
+        RerollShop.freeRerollsValue += rerollAmount;
+    }
+}
