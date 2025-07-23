c5798fcbb0ab82305cfe0df6ac9bde6fb22f90b2
Juanda999 <juanda999games@gmail.com>
2025-07-23 17:53:42 -0500

    Add interactable shop behaviour

---
+++ Assets/Scripts/Interactable/ShopItem.cs
@@ -0,0 +1,21 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopItem : MonoBehaviour
+{
+    public int ammountRequired;
+
+    [SerializeField] private TextMeshProUGUI buyLabel;
+    [SerializeField] private Button interactButton;
+    void Start()
+    {
+        UpdateBuyButtonStatus();
+    }
+
+    private void UpdateBuyButtonStatus()
+    {
+        interactButton.interactable = CurrencyManager.instance.currentCurrency > ammountRequired;
+        buyLabel.text = "Buy $" + ammountRequired;
+    }
+}
