20ba871d5c9673e08f77158529b56e667bcafd3b
Juanda999 <juanda999games@gmail.com>
2025-07-23 21:43:36 -0500

    Spawn packs on the shop

--- Assets/Scripts/Managers/Shop/JokerSpawner.cs
+++ Assets/Scripts/Managers/Shop/JokerSpawner.cs
@@ -1,15 +1,28 @@
+using System.Collections;
 using UnityEngine;
 
 public class JokerSpawner : MonoBehaviour
 {
     public JokerData[] allJokers;
     [SerializeField] private int defaultJokersAtTime = 2;
+
+    [Header("Joker Spawning")]
     [SerializeField] private Transform jokerTransform;
     [SerializeField] private GameObject jokerPrefab;
 
+    [Header("Packs Database")]
+    [SerializeField] private Transform packsTransform;
+    [SerializeField] private GameObject shopPackPrefab;
     void OnEnable()
     {
+        StartCoroutine(GenerateStuff());
+    }
+
+    private IEnumerator GenerateStuff()
+    {
+        yield return new WaitForSeconds(0.1f);
         GenerateJokers();
+        GenerateShopPacks();
     }
     public void GenerateJokers()
     {
@@ -28,5 +41,24 @@ public class JokerSpawner : MonoBehaviour
         }
 
         gameObject.SetActive(false);
+    }
+
+    private void GenerateShopPacks()
+    {
+        Transform[] packs = packsTransform.GetComponentsInChildren<Transform>();
+        if (packs.Length > 1)
+        {
+            for (int i = 1; i < packs.Length; i++)
+            {
+                Destroy(packs[i].gameObject);
+            }
+        }
+
+
+        for (int i = 0; i < 2; i++)
+        {
+            GameObject shopPack = Instantiate(shopPackPrefab, packsTransform);
+            shopPack.GetComponent<ShopItem>().SetPackData(DatabaseManager.instance.shopPacksDatabase.GetRandomBuffonPack());
+        }
     }
 }
