17f02b2bcdd5dcf29a4cde247e745cf48b168e36
Juanda999 <juanda999games@gmail.com>
2025-07-25 17:57:49 -0500

    Add version text

--- Assets/Scripts/Managers/Menu/Menu_Manager.cs
+++ Assets/Scripts/Managers/Menu/Menu_Manager.cs
@@ -1,10 +1,16 @@
+using TMPro;
 using UnityEngine;
 
 public class Menu_Manager : MonoBehaviour
 {
     [SerializeField] private string bugReportLink;
     [SerializeField] private string ideaFormLink;
+    [SerializeField] private TextMeshProUGUI versionText;
 
+    void Start()
+    {
+        versionText.text = Application.version;
+    }
     public void GoToBugReport()
     {
         Application.OpenURL(bugReportLink);
