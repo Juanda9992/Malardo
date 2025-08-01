35e9991f48005126897d1c9cb531ae87c92867a6
Juanda999 <juanda999games@gmail.com>
2025-07-31 22:06:47 -0500

    Add blind to debuff suit

---
+++ Assets/Scripts/Blind/DebuffSuitBlind.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Blind", menuName = "Scriptables/Blind/Debuff Suit")]
+public class DebuffSuitBlind : CurrentBlind
+{
+    public static Action<Suit,bool> OnDebuffBlindStatus;
+    public static Suit suitDebuffed = Suit.None;
+    public Suit suitToDebuff = Suit.None;
+
+    void Awake()
+    {
+        suitDebuffed = Suit.None;
+    }
+    [ContextMenu("Test Effect")]
+    public override void ApplyEffect()
+    {
+        OnDebuffBlindStatus?.Invoke(suitToDebuff,true);
+        suitDebuffed = suitToDebuff;
+        Debug.Log(suitToDebuff + " Debuffed");
+    }
+    [ContextMenu("Revert Effect")]
+    public override void RevertEffect()
+    {
+        suitDebuffed = Suit.None;;
+        OnDebuffBlindStatus?.Invoke(suitDebuffed,false);
+        Debug.Log(suitToDebuff + " Debuffed");
+    }
+}
