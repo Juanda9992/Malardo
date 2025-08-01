8c16acad120de5fb70b4ded07ff14fda03bdf0f1
Juanda999 <juanda999games@gmail.com>
2025-07-31 23:25:46 -0500

    Add field for sprite and bg color to the blind

--- Assets/Scripts/Blind/CurrentBlind.cs
+++ Assets/Scripts/Blind/CurrentBlind.cs
@@ -4,6 +4,8 @@ public abstract class CurrentBlind : ScriptableObject
 {
     public string blindName;
     public string blindDescription;
+    public Sprite blindSprite;
+    public Color blindColor;
     public abstract void ApplyEffect();
     public abstract void RevertEffect();
 }
