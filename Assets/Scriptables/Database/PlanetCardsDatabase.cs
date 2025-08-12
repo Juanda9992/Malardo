9485b96b2e9fd45dd7da8cd4be0d94e77756d35f
Juanda999 <juanda999games@gmail.com>
2025-08-12 17:42:11 -0500

    Add database that stores planet cards

---
+++ Assets/Scriptables/Database/PlanetCardsDatabase.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Planet Card Database", menuName = "Scriptables/Database/Planet Card Database")]
+public class PlanetCardsDatabase : ScriptableObject
+{
+    public List<PlanetCardData> allPlanetCards;
+
+    public PlanetCardData GetRandomPlanetCard()
+    {
+        return allPlanetCards[Random.Range(0,allPlanetCards.Count)];
+    }
+}
