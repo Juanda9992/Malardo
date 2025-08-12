e47e2a701e4dd65675933afe9ca0d149a7cf5b09
Juanda999 <juanda999games@gmail.com>
2025-08-12 18:53:46 -0500

    Create base consumable

--- Assets/Scripts/Visuals/InteractButton.cs
+++ Assets/Scripts/Visuals/InteractButton.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class InteractButton : MonoBehaviour
 {
     public GameObject interactButton;
+    public GameObject sellButton;
 
     private bool selected;
 
@@ -11,11 +12,17 @@ public class InteractButton : MonoBehaviour
         selected = !selected;
 
         interactButton.SetActive(selected);
+
+        if (sellButton == null) return;
+        sellButton.SetActive(selected);
     }
 
     public void SetSelection(bool status)
     {
         selected = status;
         interactButton.SetActive(status);
+
+        if (sellButton == null) return;
+        sellButton.SetActive(selected);
     }
 }
