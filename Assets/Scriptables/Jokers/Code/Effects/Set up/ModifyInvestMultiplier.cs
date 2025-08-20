b115f091354578ab6df8987f7c30ab07df43d031
Juanda999 <juanda999games@gmail.com>
2025-08-19 21:55:20 -0500

    Set invest multiplier joker effect

---
+++ Assets/Scriptables/Jokers/Code/Effects/Set up/ModifyInvestMultiplier.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+[CreateAssetMenu(fileName = "Modify Invest Multiplier",menuName = "Scriptables/Joker/Effect/Set up/Modify Invest Multiplier")]
+public class ModifyInvestMultiplier : JokerEffect
+{
+    public bool increase;
+
+    public override void ApplyEffect()
+    {
+        if (increase)
+        {
+            CurrencyScreenManager.instance.investMultiplier++;
+        }
+        else
+        {
+            CurrencyScreenManager.instance.investMultiplier--;
+        }
+    }
+}
