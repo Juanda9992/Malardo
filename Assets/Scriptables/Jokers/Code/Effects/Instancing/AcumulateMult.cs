1860747ef5eeced60680d4ce255fa166e59d5cfb
Juanda999 <juanda999games@gmail.com>
2025-08-25 22:32:24 -0500

    Add effect for accumulate mult

---
+++ Assets/Scriptables/Jokers/Code/Effects/Instancing/AcumulateMult.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Acumulate Mult",menuName = "Scriptables/Joker/Effect/Instance Effects/Acumulate Mult")]
+public class AcumulateMult : JokerEffect
+{
+    public int multAmmount;
+    public override void ApplyEffect(JokerInstance instance)
+    {
+        instance.totalMult += multAmmount;
+        UpdateDescription(instance);
+        instance.triggerMessage = "+" + multAmmount.ToString();
+        instance.jokerContainer.TriggerMessage();
+    }
+
+    public override void UpdateDescription(JokerInstance instance)
+    {
+        instance.jokerDescription = instance.data.description.Replace("_R_", instance.totalMult.ToString());
+    }
+
+
+}
