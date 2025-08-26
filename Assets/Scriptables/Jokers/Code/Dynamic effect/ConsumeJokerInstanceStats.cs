c69d890054bffe55212983a7fc13a512776faa18
Juanda999 <juanda999games@gmail.com>
2025-08-25 22:32:37 -0500

    Add effect for consuming joker mult

--- Assets/Scriptables/Jokers/Code/Dynamic effect/ConsumeJokerInstanceStats.cs
+++ Assets/Scriptables/Jokers/Code/Dynamic effect/ConsumeJokerInstanceStats.cs
@@ -3,12 +3,19 @@ using UnityEngine;
 public class ConsumeJokerInstanceStats : JokerEffect
 {
     public bool chips;
+    public bool mult;
     public override void ApplyEffect(JokerInstance instance)
     {
         if (chips)
         {
             ScoreManager.instance.AddChips(instance.totalChips);
             instance.triggerMessage = "+" + instance.totalChips;
+        }
+
+        if (mult)
+        {
+            ScoreManager.instance.AddChips(instance.totalMult);
+            instance.triggerMessage = "+" + instance.totalMult;
         }
 
         instance.jokerContainer.TriggerMessage();
