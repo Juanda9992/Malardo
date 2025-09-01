c2afdb84e134d6dc015ed11343e8b8cbc734a6eb
Juanda999 <juanda999games@gmail.com>
2025-08-31 23:23:03 -0500

    Add logic for displaying extra tag on the joker description

--- Assets/Scriptables/Database/CardColorDatabase.cs
+++ Assets/Scriptables/Database/CardColorDatabase.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Color Database", menuName = "Scriptables/Database/Color Database")]
@@ -10,6 +11,9 @@ public class CardColorDatabase : ScriptableObject
     public Color luckyCard;
     public Color glassCard;
 
+    [Header("Description Colors")]
+    public List<DescriptionColor> descriptionColors;
+
     [Header("Card Edition Colors")]
     public Color foilColor;
     public Color holographicColor;
@@ -18,4 +22,31 @@ public class CardColorDatabase : ScriptableObject
     public Color buffonPackBgColor;
     public Color cardPackBgColor;
     public Color planetPackBgColor;
+}
+[System.Serializable]
+public class DescriptionColor
+{
+    public DescriptionType descriptionType;
+    public Color instanceColor;
+    public static DescriptionType GetTypeByjokerRarity(JokerInstance jokerInstance)
+    {
+        if (jokerInstance.data.jokerRarity == JokerRarity.Common)
+        {
+            return DescriptionType.Common;
+        }
+        else if (jokerInstance.data.jokerRarity == JokerRarity.Uncommon)
+        {
+            return DescriptionType.Uncommon;
+        }
+        else if (jokerInstance.data.jokerRarity == JokerRarity.Rare)
+        {
+            return DescriptionType.Rare;
+        }
+        else if (jokerInstance.data.jokerRarity == JokerRarity.Legendary)
+        {
+            return DescriptionType.Legendary;
+        }
+
+        return DescriptionType.None;
+    }
 }
--- Assets/Scripts/Joker/JokerDescription.cs
+++ Assets/Scripts/Joker/JokerDescription.cs
@@ -1,26 +1,21 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class JokerDescription : MonoBehaviour
 {
     public static JokerDescription instance;
 
+    [SerializeField] private TextMeshProUGUI nameText, descriptionText;
+    [SerializeField] private float cardDescriptionOffsetY;
+    [SerializeField] private Image rarityBgColor;
+    [SerializeField] private TextMeshProUGUI rarityText;
+    [SerializeField] private GameObject rarityObj;
     void Awake()
     {
         instance = this;
         SetDescriptionOff();
     }
-    [SerializeField] private TextMeshProUGUI nameText, descriptionText;
-
-    [SerializeField] private float cardDescriptionOffsetY;
-    public void SetDescriptionOn(JokerInstance jokerData, Vector2 jokerPos)
-    {
-        transform.localScale = Vector2.one;
-        nameText.text = jokerData.data.jokerName;
-        descriptionText.text = jokerData.jokerDescription;
-        transform.position = jokerPos;
-    }
-
     public void SetDescriptionOff()
     {
         transform.localScale = Vector2.zero;
@@ -32,13 +27,49 @@ public class JokerDescription : MonoBehaviour
         transform.localScale = Vector2.one;
         transform.position = (Vector2)card_Data.transform.position + new Vector2(0, cardDescriptionOffsetY);
         descriptionText.text = $"+{card_Data.currentCard.chipAmmount} chips";
+        SetDescriptionRarity(DescriptionType.None);
     }
 
-    public void SetGenericDescription(string itemName, string itemDescription, Vector2 itemPosition)
+    public void SetGenericDescription(string itemName, string itemDescription, Vector2 itemPosition, DescriptionType descriptionType)
     {
         nameText.text = itemName;
         descriptionText.text = itemDescription;
         transform.position = itemPosition;
         transform.localScale = Vector2.one;
+
+        SetDescriptionRarity(descriptionType);
+
     }
+
+    private void SetDescriptionRarity(DescriptionType descriptionType)
+    {
+        if (descriptionType == DescriptionType.None)
+        {
+            rarityObj.SetActive(false);
+            return;
+        }
+
+
+        rarityObj.SetActive(true);
+        DescriptionColor descriptionColor = DatabaseManager.instance.cardColorDatabase.descriptionColors.Find(x => x.descriptionType == descriptionType);
+
+        rarityBgColor.color = descriptionColor.instanceColor;
+        rarityText.text = descriptionColor.descriptionType.ToString();
+    }
+
+
+}
+
+public enum DescriptionType
+{
+    None = -1,
+    Common = 0,
+    Uncommon = 1,
+    Rare = 2,
+    Legendary = 3,
+    Booster = 4,
+    Planet = 5,
+    Tarot = 6,
+    Spectral = 7,
+    Voucher = 8
 }
