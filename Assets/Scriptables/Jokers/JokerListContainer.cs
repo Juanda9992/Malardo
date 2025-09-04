8831d107ac58d8f4d1d67dbfb3952e09acc91164
Juanda999 <juanda999games@gmail.com>
2025-09-03 22:38:38 -0500

    Add method for get random joker by rarity

--- Assets/Scriptables/Jokers/JokerListContainer.cs
+++ Assets/Scriptables/Jokers/JokerListContainer.cs
@@ -1,6 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
-[CreateAssetMenu(fileName = "Joker List Data",menuName = "Scriptables/Joker List Data")]
+[CreateAssetMenu(fileName = "Joker List Data", menuName = "Scriptables/Joker List Data")]
 public class JokerListContainer : ScriptableObject
 {
     public List<JokerData> allJokersInGame;
@@ -8,5 +8,11 @@ public class JokerListContainer : ScriptableObject
     public JokerInstance GetRandomJoker()
     {
         return new JokerInstance(allJokersInGame[Random.Range(0, allJokersInGame.Count)]);
+    }
+
+    public JokerInstance GetRandomJokerByRarity(JokerRarity jokerRarity)
+    {
+        List<JokerData> jokerData = allJokersInGame.FindAll(x => x.jokerRarity == jokerRarity);
+        return new JokerInstance(jokerData[Random.Range(0, jokerData.Count)]);
     }
 }
