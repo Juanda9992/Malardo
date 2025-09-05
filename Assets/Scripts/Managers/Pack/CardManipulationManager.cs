a0cbf850b1745093b7824c57d115fcd4d975dd54
Juanda999 <juanda999games@gmail.com>
2025-09-04 23:55:04 -0500

    Add card manipulation manager

---
+++ Assets/Scripts/Managers/Pack/CardManipulationManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardManipulationManager : MonoBehaviour
+{
+    [SerializeField] private Transform cardEffectParent;
+    [SerializeField] private Transform handContainer;
+    [SerializeField] private HorizontalLayoutGroup layoutGroup;
+    [SerializeField] private GameObject packinteractable;
+    public void SetCardLogic(int cards)
+    {
+        for (int i = 0; i < cards; i++)
+        {
+            GameObject pack = Instantiate(packinteractable, cardEffectParent);
+            pack.GetComponent<PackInteractable>().SetTarotData(DatabaseManager.instance.tarotCardDatabase.GetRandomTarotCard());
+        }
+
+        DeckManager.instance.RegenerateRoundDeck();
+        List<Card> roundDeck = DeckManager.instance.GetRoundDeck();
+
+        Card generatedCard;
+        for (int i = 0; i < DeckManager.instance.currentHandSize; i++)
+        {
+            generatedCard = roundDeck[Random.Range(0, roundDeck.Count)];
+            CardManager.instance.GenerateCardOnHand(generatedCard, handContainer,layoutGroup);
+        }
+    }
+}
