1ca98de794b7b59e841569641f7bc96a6e42ab43
Juanda999 <juanda999games@gmail.com>
2025-09-11 11:45:06 -0500

    Add effect for creating spectral card

--- Assets/Scriptables/Jokers/Code/Effects/Upgrades/CreateTarotCard.cs
+++ Assets/Scriptables/Jokers/Code/Effects/Upgrades/CreateTarotCard.cs
@@ -3,11 +3,19 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Create Tarot Card",menuName = "Scriptables/Joker/Effect/Upgrade/Create Tarot Card")]
 public class CreateTarotCard : JokerEffect
 {
+    public bool isTarot;
     public override void ApplyEffect(JokerInstance instance)
     {
         if (ConsumableManager.instance.CanAddConsumable)
         {
-            ConsumableManager.instance.GenerateTarotCard(DatabaseManager.instance.tarotCardDatabase.GetRandomTarotCard());
+            if (isTarot)
+            {
+                ConsumableManager.instance.GenerateTarotCard(DatabaseManager.instance.tarotCardDatabase.GetRandomTarotCard());
+            }
+            else
+            {
+                ConsumableManager.instance.GenerateTarotCard(DatabaseManager.instance.tarotCardDatabase.GetRandomSpectralCard());
+            }
             instance.triggerMessage = "Create!";
             instance.jokerContainer.TriggerMessage();
         }
