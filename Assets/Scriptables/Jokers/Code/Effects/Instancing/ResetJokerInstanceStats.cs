3b140ec7f062b020b1f36d4875b00c9acf3c507f
Juanda999 <juanda999games@gmail.com>
2025-09-11 12:17:09 -0500

    Add hit the road joker

--- Assets/Scriptables/Jokers/Code/Effects/Instancing/ResetJokerInstanceStats.cs
+++ Assets/Scriptables/Jokers/Code/Effects/Instancing/ResetJokerInstanceStats.cs
@@ -1,17 +1,34 @@
 using UnityEngine;
-[CreateAssetMenu(fileName = "Reset Stats",menuName = "Scriptables/Joker/Effect/Instance Effects/ Reset Stats")]
+[CreateAssetMenu(fileName = "Reset Stats", menuName = "Scriptables/Joker/Effect/Instance Effects/ Reset Stats")]
 public class ResetJokerInstanceStats : JokerEffect
 {
     public bool mult;
+    public bool multiplier;
     public override void ApplyEffect(JokerInstance instance)
     {
         if (mult)
         {
             instance.totalMult = 0;
-
+        }
+        if (multiplier)
+        {
+            instance.totalMultiplier = 1;
         }
 
         instance.triggerMessage = "Reset!";
         instance.jokerContainer.TriggerMessage();
+        UpdateDescription(instance);
+    }
+
+    public override void UpdateDescription(JokerInstance instance)
+    {
+        if (mult)
+        {
+            instance.jokerDescription = instance.data.description.Replace("_R_", instance.totalMult.ToString());
+        }
+        if (multiplier)
+        {
+            instance.jokerDescription = instance.data.description.Replace("_R_",instance.totalMultiplier.ToString());
+        }
     }
 }
