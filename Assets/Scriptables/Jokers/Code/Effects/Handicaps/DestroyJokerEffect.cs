22a5332307f7007fe7741c06d6ca75dbf69b8447
Juanda999 <juanda999games@gmail.com>
2025-09-11 22:38:49 -0500

    Add joker effect for destroying random joker

--- Assets/Scriptables/Jokers/Code/Effects/Handicaps/DestroyJokerEffect.cs
+++ Assets/Scriptables/Jokers/Code/Effects/Handicaps/DestroyJokerEffect.cs
@@ -4,7 +4,37 @@ using UnityEngine;
 public class DestroyJokerEffect : JokerEffect
 {
     public bool right;
+    public bool random;
     public override void ApplyEffect(JokerInstance instance)
+    {
+        if (right)
+        {
+            DestroyJokerAtRight(instance);
+        }
+
+        if (random)
+        {
+            DestroyRandomJoker(instance);
+        }
+    }
+
+    private void DestroyRandomJoker(JokerInstance instance)
+    {
+        if (JokerManager.instance.currentJokers.Count > 1)
+        {
+            JokerContainer jokerContainer;
+
+            do
+            {
+                jokerContainer = JokerManager.instance.currentJokers[Random.Range(0, JokerManager.instance.currentJokers.Count)];
+            } while (GameObject.ReferenceEquals(jokerContainer,instance.jokerContainer));
+
+            JokerManager.instance.RemoveJoker(jokerContainer);
+
+        }
+    }
+
+    private void DestroyJokerAtRight(JokerInstance instance)
     {
         int siblingIndex = instance.jokerContainer.transform.GetSiblingIndex();
         if (siblingIndex < JokerManager.instance.currentJokers.Count - 1)
@@ -19,7 +49,6 @@ public class DestroyJokerEffect : JokerEffect
             UpdateDescription(instance);
         }
     }
-
     public override void UpdateDescription(JokerInstance instance)
     {
         instance.jokerDescription = instance.data.description.Replace("_R_", instance.totalMult.ToString());
