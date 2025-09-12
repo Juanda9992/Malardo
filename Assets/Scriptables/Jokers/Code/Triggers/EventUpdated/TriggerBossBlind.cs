fca9edfdfb59f0677d8ed759d5bbee1a77f5b6b2
Juanda999 <juanda999games@gmail.com>
2025-09-11 22:38:34 -0500

    Add trigger for unactive boss  blind

--- Assets/Scriptables/Jokers/Code/Triggers/EventUpdated/TriggerBossBlind.cs
+++ Assets/Scriptables/Jokers/Code/Triggers/EventUpdated/TriggerBossBlind.cs
@@ -3,8 +3,16 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Boss Blind Active",menuName = "Scriptables/Joker/Trigger/Require Boss Blind")]
 public class TriggerBossBlind : JokerTrigger
 {
+    public bool active;
     public override bool MeetCondition(GameStatus gameStatus)
     {
-        return BlindManager.instance.activeBossBlind != null;
+        if (active)
+        {
+            return BlindManager.instance.activeBossBlind != null;
+        }
+        else
+        {
+            return BlindManager.instance.activeBossBlind == null;
+        }
     }
 }
