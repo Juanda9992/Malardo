3b23deba7f6e2b6be1703c94f6c633d4c654080a
Juanda999 <juanda999games@gmail.com>
2025-09-17 22:42:15 -0500

    Add logic for rerolling the boss blind

---
+++ Assets/Scripts/Blind/BlindReroll.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BlindReroll : MonoBehaviour
+{
+    [SerializeField] private BlindSelector blindSelector;
+    public static BlindReroll blindReroll;
+
+    [SerializeField] private int blindRerollCost = 10;
+    [SerializeField] private Button rerollButton;
+    public bool canReroll = false;
+    public bool infiniteRerolls = false;
+
+    void Awake()
+    {
+        blindReroll = this;
+        rerollButton.gameObject.SetActive(false);
+    }
+
+    public void EnableBlindReroll()
+    {
+        canReroll = true;
+        rerollButton.gameObject.SetActive(true);
+        SetRerollButtonState();
+    }
+
+    public void RerollBlind()
+    {
+        CurrencyManager.instance.RemoveCurrency(blindRerollCost);
+        blindSelector.PickUpRandomBossBlind();
+
+        rerollButton.gameObject.SetActive(infiniteRerolls);
+        SetRerollButtonState();
+    }
+
+    private void SetRerollButtonState()
+    {
+        rerollButton.interactable = CurrencyManager.instance.currentCurrency >= blindRerollCost;
+    }
+
+    private void ListenForCurrencyChange(int newCurrency)
+    {
+        SetRerollButtonState();
+    }
+
+    void OnEnable()
+    {
+        CurrencyManager.OnMoneyChanged += ListenForCurrencyChange; 
+    }
+
+}
