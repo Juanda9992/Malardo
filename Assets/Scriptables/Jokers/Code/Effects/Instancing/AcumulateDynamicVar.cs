f1daf75541ff3e01ff867e1dfd34a8d8416f03b3
Juanda999 <juanda999games@gmail.com>
2025-09-18 17:38:34 -0500

    Add effect for acumulating dynamic var

---
+++ Assets/Scriptables/Jokers/Code/Effects/Instancing/AcumulateDynamicVar.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+[CreateAssetMenu(fileName =  "Add Dynamic Var",menuName = "Scriptables/Joker/Effect/Instance Effects/Increase Dynamic Var")]
+public class AcumulateDynamicVar : JokerEffect
+{
+    public int increaseAmmount;
+    public override void ApplyEffect(JokerInstance instance)
+    {
+        instance.dynamicVariable += increaseAmmount;
+        instance.triggerMessage = "+" + increaseAmmount;
+        instance.jokerContainer.TriggerMessage();
+    }
+
+    public override void UpdateDescription(JokerInstance instance)
+    {
+        instance.jokerDescription = instance.data.description.Replace("_R_",instance.dynamicVariable.ToString());
+    }
+    public override void SetupEffect(JokerInstance jokerInstance)
+    {
+        UpdateDescription(jokerInstance);
+    }
+}
