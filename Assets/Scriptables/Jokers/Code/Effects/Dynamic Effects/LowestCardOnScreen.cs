7ad41c0664888aa2cd920d51ef7921657b00c41f
Juanda999 <juanda999games@gmail.com>
2025-09-22 15:48:02 -0500

    FIx bug with raised fist not giving mult
    Improved visual effects

--- Assets/Scriptables/Jokers/Code/Effects/Dynamic Effects/LowestCardOnScreen.cs
+++ Assets/Scriptables/Jokers/Code/Effects/Dynamic Effects/LowestCardOnScreen.cs
@@ -4,17 +4,34 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Lowest Card", menuName = "Scriptables/Joker/Effect/Lowest Card to Mult")]
 public class LowestCardOnScreen : JokerEffect
 {
-    int lowestCardNumber;
+    Card lowestCard;
     public override void ApplyEffect(JokerInstance jokerInstance)
     {
-        ScoreManager.instance.AddMult(lowestCardNumber);
+        string trigger;
+        if (Calculate() > 0)
+        {
+            ScoreManager.instance.AddMult(Calculate());
+            trigger = "+" + Calculate();
+        }
+        else
+        {
+            trigger = "Disabled!";
+        }
+        ScoreSign.instance.SetMessage(Color.red, trigger, lowestCard.linkedCard.transform.position);
+        lowestCard.linkedCard.pointerInteraction.ShakeCard();
     }
-
-    public override string GetCustomMessage()
+    public int Calculate()
     {
-        lowestCardNumber = (CardManager.instance.cardsOnScreen.OrderBy(x => x.currentCard.number).ToList()[0].currentCard.number) *2;
-        return "+" + lowestCardNumber.ToString();
-    }
+        lowestCard = CardManager.instance.cardsOnScreen.OrderBy(x => x.currentCard.number).ToList()[0].currentCard;
 
+        if (lowestCard.canPlay)
+        {
+            return lowestCard.number * 2;
+        }
+        else
+        {
+            return -1;
+        }
+    }
 
 }
