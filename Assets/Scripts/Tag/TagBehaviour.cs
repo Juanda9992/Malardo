3699b036044205b68e5457b7f1ade8af7dc8776e
Juanda999 <juanda999games@gmail.com>
2025-09-29 00:19:58 -0500

    Add logic to set up tag sprites

--- Assets/Scripts/Tag/TagBehaviour.cs
+++ Assets/Scripts/Tag/TagBehaviour.cs
@@ -1,13 +1,15 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TagBehaviour : MonoBehaviour
 {
     [SerializeField] private TagData testTag;
-
+    [SerializeField] private Image tagSprite;
     private TagData currentTag;
     public void SetTagData(TagData tagData)
     {
         currentTag = tagData;
+        tagSprite.sprite = currentTag.tagSprite;
         GetComponent<DescriptionContainer>().SetNameAndDescription(tagData.tagName, tagData.tagDescription, DescriptionType.None);
     }
 
